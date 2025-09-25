7345161217715ee8f81ba1b53d27b3c8d647be5e
clement jacquet <clement.jacquet.16@gmail.com>
2025-09-25 16:13:55 +0200

    now you can sign in sign up, add avatar, avatar is validated by content type

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :authenticate_user!, only: :dashboard
 
   def home
   end
+
+  def dashboard
+  end
+
 end
