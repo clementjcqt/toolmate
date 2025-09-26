6e5026896636b9d491e2ba6a408f4c5d2d4ae91b
clement jacquet <clement.jacquet.16@gmail.com>
2025-09-26 11:25:32 +0200

    added geocoder, user validations, updated users table to add precise addres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
 p "destroying all users"
 User.destroy_all
 p "users destroyed"
+
+user.create(
+  email: "clement@clement.fr",
+  password: "password123",
+  bio: "Hello world",
+  street: "41 Rue petit parc",
+  city: "Bordeaux",
+  post_code: 33_200
+)
