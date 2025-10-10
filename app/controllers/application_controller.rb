28daa0ef242bfe59ca82ec7492c7e3d3ae2b09be
clement jacquet <clement.jacquet.16@gmail.com>
2025-10-10 10:54:35 +0200

    added a log out button on dashboard, fixed route to log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(_resource_or_scope)
+    home_path
+  end
+
   protected
 
   def configure_permitted_parameters
